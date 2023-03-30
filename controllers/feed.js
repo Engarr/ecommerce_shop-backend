02fde4875b6f62af34bf9977f141bd8f625c15fa
Łukasz <lukasz.poczta12@gmail.com>
2023-03-30 16:22:00 +0200

    added new fetching product routes

--- controllers/feed.js
+++ controllers/feed.js
@@ -14,7 +14,12 @@ exports.getUser = async (req, res, next) => {
 			error.statusCode = 422;
 			throw error;
 		}
-		res.status(200).json({ message: 'user fetched', userData: user });
+		const userProductsId = user.products;
+		const products = await Product.find({ _id: { $in: userProductsId } });
+
+		res
+			.status(200)
+			.json({ message: 'user fetched', userData: user, products: products });
 	} catch (err) {
 		if (!err) {
 			err.statusCode = 500;
@@ -53,6 +58,35 @@ exports.postAddProduct = async (req, res, next) => {
 		res.status(200).json({ message: 'product has been created', data: result });
 	} catch (err) {
 		if (!err) {
+			err.statusCode = 500;
+		}
+		next(err);
+	}
+};
+
+exports.getProducts = async (req, res, next) => {
+	try {
+		const products = await Product.find();
+		res
+			.status(200)
+			.json({ message: 'Fetched products successfully.', products: products });
+	} catch (err) {
+		if (!err.statusCode) {
+			err.statusCode = 500;
+		}
+		next(err);
+	}
+};
+
+exports.getCategoryProducts = async (req, res, next) => {
+	const category = req.params.category;
+	try {
+		const products = await Product.find({ category: category });
+		res
+			.status(200)
+			.json({ message: 'Fetched products successfully.', products: products });
+	} catch (err) {
+		if (!err.statusCode) {
 			err.statusCode = 500;
 		}
 		next(err);
--- router/feed.js
+++ router/feed.js
@@ -4,5 +4,7 @@ const feedController = require('../controllers/feed');
 
 router.get('/user/:userId', feedController.getUser);
 router.post('/add-product', feedController.postAddProduct);
+router.get('/products', feedController.getProducts);
+router.get('/products/:category', feedController.getCategoryProducts);
 
 module.exports = router;
